8aad382d36cda3487e9496d80dda7af69bcae4c5
Tom Clacy <clacyt@uni.coventry.ac.uk>
2016-02-01 15:43:52 +0000

    Fixed issue with assigning entity to player

--- KillStreak/src/me/biiing/killstreak/listeners/PlayerKill.java
+++ KillStreak/src/me/biiing/killstreak/listeners/PlayerKill.java
@@ -2,6 +2,7 @@ package me.biiing.killstreak.listeners;
 
 import me.biiing.killstreak.KillStreak;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,15 +17,16 @@ public class PlayerKill implements Listener {
 	
 	@EventHandler
 	public void onPlayerKill(EntityDeathEvent e) {
-		Player p = e.getEntity().getKiller();
-		if (p instanceof Player) {
-			String pUUID = p.getUniqueId().toString();
-			int kills = plugin.getConfig().getInt("Players." + pUUID + ".Kills");
-			int streak = plugin.getConfig().getInt("Players." + pUUID + ".Streak");
-			plugin.getConfig().set("Players." + pUUID + ".Kills", kills +1);
-			plugin.getConfig().set("Players." + pUUID + ".Streak", streak +1);
-			plugin.saveConfig();
-			p.sendMessage("Win?!");	
+		Player k = e.getEntity().getKiller();
+		if (k instanceof Player) {
+			if (e.getEntity() instanceof Player) {
+				String pUUID = k.getUniqueId().toString();
+				int kills = plugin.getConfig().getInt("Players." + pUUID + ".Kills");
+				int streak = plugin.getConfig().getInt("Players." + pUUID + ".Streak");
+				plugin.getConfig().set("Players." + pUUID + ".Kills", kills +1);
+				plugin.getConfig().set("Players." + pUUID + ".Streak", streak +1);
+				plugin.saveConfig();
+			}
 		}
 	}
 }
