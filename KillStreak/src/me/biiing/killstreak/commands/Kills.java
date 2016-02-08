dfe1c207e8a51704bb7e5af4b573eab04fe38557
Tom Clacy <clacyt@uni.coventry.ac.uk>
2016-02-08 22:43:03 +0000

    Added ability to check other players kills

--- KillStreak/src/me/biiing/killstreak/commands/Kills.java
+++ KillStreak/src/me/biiing/killstreak/commands/Kills.java
@@ -1,5 +1,7 @@
 package me.biiing.killstreak.commands;
 
+import java.util.UUID;
+
 import me.biiing.killstreak.KillStreak;
 
 import org.bukkit.ChatColor;
@@ -20,12 +22,41 @@ public class Kills implements CommandExecutor{
 		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			if (cmd.getName().equalsIgnoreCase("kills")) {
-				String uuid = p.getUniqueId().toString();
-				int kills = plugin.getConfig().getInt("Players." + uuid + ".Kills");
-				p.sendMessage(ChatColor.GOLD + "You have killed " + ChatColor.DARK_RED + kills + ChatColor.GOLD + " players");
-				return true;
+				if (args.length == 0) {
+					String uuid = p.getUniqueId().toString();
+					int kills = plugin.getConfig().getInt("Players." + uuid + ".Kills");
+					p.sendMessage(ChatColor.GOLD + "You have killed " + ChatColor.DARK_RED + kills + ChatColor.GOLD + " players");
+					return true;
+				}
+				if (args.length == 1) {
+					Player t = plugin.getServer().getPlayer(args[0]);
+					// If player is Offline
+					if (t == null) {
+						UUID UUID = plugin.getServer().getOfflinePlayer(args[0]).getUniqueId();
+						String tUUID = UUID.toString(); 
+						if (plugin.getConfig().contains("Players." + tUUID)){
+							int kills = plugin.getConfig().getInt("Players." + tUUID + ".Kills");
+							sender.sendMessage(ChatColor.GOLD + "" + kills);
+						} else {
+							sender.sendMessage(ChatColor.RED + "Player '" + args[0] + "' not found");
+						}
+						return true;
+					} 
+					// If Player is Online
+					if (t.isOnline()) {
+						UUID UUID = plugin.getServer().getPlayer(args[0]).getUniqueId();
+						String tUUID = UUID.toString(); 
+						if (plugin.getConfig().contains("Players." + tUUID)){ 
+							int kills = plugin.getConfig().getInt("Players." + tUUID + ".Kills");
+							sender.sendMessage(ChatColor.GOLD + "" + kills);
+						} else {
+							sender.sendMessage(ChatColor.RED + "Player '" + args[0] + "' not found");
+						}
+						return true;
+				}
 			}
 		}
+	}
 		return false;
 	}
 
